76a2992c6cee379a46c04822959076dfed481dcc
Joel Llaury <joell@vt.edu>
2024-02-27 23:52:20 -0500

    Making sure all changes are commited. App and utils were excluded.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./App.css"
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainMenu from './components/MainMenu';
-import LocalGame from './components/LocalGame';
+import MainMenu from './components/MainMenu/MainMenu';
+import LocalGame from './components/LocalGame/LocalGame';
 
 function App() {
   return (
