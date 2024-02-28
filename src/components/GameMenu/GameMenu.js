1169c985db23d6da7e91a5d28d69f380f6c94003
Joel Llaury <joell@vt.edu>
2024-02-28 00:40:54 -0500

    Updated the GameMenu to properly count down.

--- src/components/GameMenu/GameMenu.js
+++ src/components/GameMenu/GameMenu.js
@@ -1,13 +1,29 @@
-import React from "react";
-import "./GameMenu.css"; // Assuming you will create a CSS file for styling
+import React, { useEffect, useState } from "react";
+import "./GameMenu.css";
 
 const GameMenu = () => {
-    // Use hooks for the timer object
+    const [timeLeft, setTimeLeft] = useState(15);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTimeLeft(timeLeft - 1);
+        }, 1000);
+
+        if (timeLeft === 0) {
+            clearInterval(timer);
+            console.log("Time's up");
+        }
+
+        // Clears interval on dismount
+        return () => {
+            clearInterval(timer);
+        }
+    }, [timeLeft])
 
     return (
         <div className="game-menu">
             <button className="game-menu-button" aria-label="Forfeit Game">Forfeit</button>
-            <label>0:30</label>
+            <label>0:{(timeLeft < 10 ? '0' : '') + timeLeft.toString()}</label>
         </div>
     );
 }
