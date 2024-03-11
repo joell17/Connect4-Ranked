3984b8b6a89a7807331768dddfa5bd94fda55a80
Joel Llaury <joell@vt.edu>
2024-03-11 00:20:28 -0400

    Got an extending side-menu working. I need to actually put stuff into it now.
    Current To Do:
    - Add options to choose a primary and secondary skin
    - Make Game Menu fixed width when in game
    	- Make children have fixed positions

--- src/components/MainMenu/MainMenu.js
+++ src/components/MainMenu/MainMenu.js
@@ -1,21 +1,37 @@
+import { useState } from "react";
 import React from "react";
 import "./MainMenu.css";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const MainMenu = () => {
   const navigate = useNavigate();
+  const [activeMenuItem, setActiveMenuItem] = useState(null);
 
   return (
-    <div className="main-menu">
-      <div className="side-menu">
+    <div className={`main-menu-top-level ${activeMenuItem ? 'active' : ''}`}>
+      <div className="main-menu">
         {/* Buttons for the side menu */}
-        <button onClick={() => navigate('/game')} className="clear-button">Local PvP</button>
+        <button onClick={() => navigate("/game")} className="clear-button">
+          Local PvP
+        </button>
         <button className="clear-button">Online Casual</button>
         <button className="clear-button">Online Ranked</button>
-        <button className="clear-button">Skins</button>
+        <button
+          onClick={() => setActiveMenuItem("skins")}
+          className="clear-button"
+        >
+          Skins
+        </button>
       </div>
+
+    
+      <div className="side-menu">
+        <button onClick={() => setActiveMenuItem(null)} className="close-button">Close</button>
+
+        {activeMenuItem === 'skins' && <div>Skins Content</div>}
+      </div>
+
       <div className="title-section">
-        {/* Title of the game */}
         <h1>Align Four</h1>
       </div>
     </div>
