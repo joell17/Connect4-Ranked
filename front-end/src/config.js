f868bd5b099dc00876edfa96207a070e67d4ffb4
lllaury <louisllaury@gmail.com>
2024-07-06 16:31:43 -0600

    Changed backend url

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -2,11 +2,11 @@ const config = {
     backendURL:
         process.env.NODE_ENV === "development"
             ? "https://localhost:3000"
-            : "https://ec2-3-133-126-162.us-east-2.compute.amazonaws.com:3000",
+            : "https://ec2-3-138-113-123.us-east-2.compute.amazonaws.com:3000",
     websocketURL:
         process.env.NODE_ENV === "development"
             ? "wss://localhost:3000"
-            : "wss://ec2-3-133-126-162.us-east-2.compute.amazonaws.com:3000",
+            : "wss://ec2-3-138-113-123.us-east-2.compute.amazonaws.com:3000",
 };
 
 console.log(process.env.NODE_ENV === "development"
